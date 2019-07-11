b5cf792a6a35623a7a9a40af12eb1a3c87bbc2f2
Thomas61170 <Thomas61170@GB-LD1LPW10G044.mastek.com>
2019-07-11 14:07:12 +0100

    New Java Project Created

---
+++ hrapp/src/main/java/com/mastek/training/hrapp/HrappApplication.java
@@ -0,0 +1,13 @@
+package com.mastek.training.hrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrappApplication.class, args);
+	}
+
+}
